157fd5ae00d55540b8687a19bb99d92c70375569
elivjo <xhafi98@gmail.com>
2021-10-03 16:22:05 +0200

    Added working list tags endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,11 @@
 from django.forms.fields import EmailField
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+def sample_user(email='test@app.com', password='testpass'):
+    """Create a helper sample user"""
+    return get_user_model().objects.create_user(email,password)
 
 
 class ModelTests(TestCase):
@@ -40,5 +45,10 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
 
-
-
+    def test_tag_str(self):
+        """test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
